a23dca6a13a158c0eda572338218cf09f34782f0
ZacharyCWilliams <zachcwilliams@icloud.com>
2019-08-20 18:35:52 -0400

    Add factory method for ShortenedUrl

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,5 +1,18 @@
+require 'securerandom'
+
 class ShortenedUrl < ApplicationRecord
   validates :short_url, presence: true, uniqueness: true
   validates :long_url, presence: true
   validates :user_id, presence: true
+
+  def self.random_code
+    loop do
+      code = SecureRandom.urlsafe_base64
+      return code unless ShortenedUrl.exists?(:short_url => code)
+    end
+  end
+
+  def self.create!(user, long_url)
+    ShortenedUrl.create(short_url: self.random_code, long_url: long_url, user_id: user.id)
+  end
 end
