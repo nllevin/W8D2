65733ef2367adba6715629c196ca91cb13d735f4
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-20 17:12:47 -0400

    Complete rails associations exercise

--- rails_associations_exercise/app/models/user.rb
+++ rails_associations_exercise/app/models/user.rb
@@ -1,2 +1,33 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+  has_many :enrollments,
+    class_name: 'Enrollment',
+    primary_key: :id,
+    foreign_key: :student_id
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+
+  has_many :taught_courses,
+    class_name: 'Course',
+    primary_key: :id,
+    foreign_key: :instructor_id
+
+  has_many :instructors,   
+    through: :enrolled_courses,
+    source: :instructor
+
+  has_many :taught_students,
+    through: :taught_courses,
+    source: :enrolled_students  
 end
